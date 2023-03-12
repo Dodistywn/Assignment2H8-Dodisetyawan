b5dbf6b4bab5fc5b746890ef4235731ed78cc900
Dodistywn <dodisetya000@gmail.com>
2023-03-12 17:29:55 +0700

    memodifikasi file script.js mencoba menampilkan inputan di form namun 'masih gagal'.

--- script.js
+++ script.js
@@ -12,3 +12,47 @@ btn.addEventListener('click', () =>{
     }
 })
 
+// tampilkan inputan form
+// function myform(){
+//     var x1 = document.getElementById("#nama").value;
+
+
+// }
+
+
+// $(document).ready(function() {
+//     $("#btn-submit").click(function() {
+//         var name = $("#nama").val()
+//         if(name === ""){
+//             alert("Silahkan isi form nama!")
+//         }else {
+//             $(".nama").html(name)
+//             $("#nama").val("")
+//         }
+//     });
+    // $("#btn-submit").click(function() {
+    //     var role = $("#role").val()
+    //     $(".role").html(role)
+    // });
+    // $("#btn-submit").click(function() {
+    //     var availability = $("#availability").val()
+    //     $(".availability").html(availability)
+    // });
+    // $("#btn-submit").click(function() {
+    //     var usia = $("#usia").val()
+    //     $(".usia").html(usia)
+    // });
+    // $("#btn-submit").click(function() {
+    //     var lokasi = $("#lokasi").val()
+    //     $(".lokasi").html(lokasi)
+    // });
+    // $("#btn-submit").click(function() {
+    //     var yearsexperience = $("#yearsexperience").val()
+    //     $(".yearsEx").html(yearsexperience)
+    // });
+    // $("#btn-submit").click(function() {
+    //     var email = $("#email").val()
+    //     $(".email").html(email)
+    // });
+   
+// });
