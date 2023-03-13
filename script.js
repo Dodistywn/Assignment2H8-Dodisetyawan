0e94858b9ad26cd23d6a30274f04b50efebba300
Dodistywn <dodisetya000@gmail.com>
2023-03-13 17:57:20 +0700

    membuat perlengkapan js untuk mnampilkan inputan dari form

--- script.js
+++ script.js
@@ -1,23 +1,81 @@
 
 // hide and show form
-let btn1 = document.querySelector('#btn-edit');
-let btn2 = document.querySelector('#btn-cancel');
-let div = document.querySelector('.container-form');
-div.style.display = "none";
+const btn1 = document.querySelector('#btn-edit');
+const btn2 = document.querySelector('#btn-cancel');
+const forms = document.querySelector('.container-form');
+forms.style.display = "none";
 
 btn1.addEventListener('click', () =>{
-    if(div.style.display === 'none'){
-        div.style.display = 'block';
+    if(forms.style.display === 'none'){
+        forms.style.display = 'block';
     }
 })
 
 btn2.addEventListener('click', () =>{
-    if(div.style.display = 'none'){
-        div.style.display === 'block';
+    if(forms.style.display = 'none'){
+        forms.style.display === 'block';
     }
 })
 
 
+// Membuat variabel global untuk mengambil data heading untuk di tampilkan di form dari id html yang di buat
+const form = document.querySelector('form');
+const namaInput = document.querySelector('#nama-input');
+const roleInput = document.querySelector('#role-input');
+const availabilityInput = document.querySelector('#availability-input');
+const usiaInput = document.querySelector('#usia-input');
+const alamatInput = document.querySelector('#alamat-input');
+const pengalamanInput = document.querySelector('#pengalaman-input');
+const emailInput = document.querySelector('#email-input');
+
+// Membuat variabel global untuk mengambil data pada heading dari id html yang di buat
+const namaTeks = document.querySelector('#nama');
+const roleTeks = document.querySelector('#role');
+const availabilityTeks = document.querySelector('#availability');
+const usiaTeks = document.querySelector('#usia');
+const alamatTeks = document.querySelector('#alamat');
+const pengalamanTeks = document.querySelector('#pengalaman');
+const emailTeks = document.querySelector('#email');
+
+// Memunculkan data pada heading ke dalam form saat button edit di klik
+function editBiodata() {
+    namaInput.value = namaTeks.textContent;
+    roleInput.value = roleTeks.textContent;
+    availabilityInput.value = availabilityTeks.textContent;
+    usiaInput.value = usiaTeks.textContent;
+    alamatInput.value = alamatTeks.textContent;
+    pengalamanInput.value = pengalamanTeks.textContent;
+    emailInput.value = emailTeks.textContent;
+}
+
+// Event listener untuk mengaktifkan button submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // mencegah halaman direfresh saat form disubmit
+
+    // Merubah data heading sesuai inputan yg di masukan di form
+    namaTeks.textContent = namaInput.value;
+    roleTeks.textContent = roleInput.value;
+    availabilityTeks.textContent = availabilityInput.value;
+    usiaTeks.textContent = usiaInput.value;
+    alamatTeks.textContent = alamatInput.value;
+    pengalamanTeks.textContent = pengalamanInput.value;
+    emailTeks.textContent = emailInput.value;
+
+    // Mengosongkan input form
+    namaInput.value = '';
+    roleInput.value = '';
+    availabilityInput.value = '';
+    usiaInput.value = '';
+    alamatInput.value = '';
+    pengalamanInput.value = '';
+    emailInput.value = '';
+
+    // Sembunyikan form setelah di-submit
+    forms.style.display = 'none';
+    biodataHeading.style.display = 'block';
+});
+
+// Mengubah kondisi agar button edit dapat mengeditan berkali kali
 
 
 // tampilkan inputan form
